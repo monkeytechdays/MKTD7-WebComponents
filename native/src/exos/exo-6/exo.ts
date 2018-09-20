de54eb9d7fcd79a807c13813d3a0d8ae4bab0735
Igor Laborie <ilaborie@gmail.com>
2018-09-21 00:42:44 +0200

    soluce exo 6

--- native/src/exos/exo-6/exo.ts
+++ native/src/exos/exo-6/exo.ts
@@ -25,7 +25,8 @@ export class Card extends HTMLElement {
 
     constructor() {
         super();
-        // TODO Step 1
+        // Step 1
+        this.attachShadow({ mode: 'open' });
     }
 
     connectedCallback() {
@@ -48,19 +49,52 @@ export class Card extends HTMLElement {
     }
 
     update() {
-        // TODO 2
+        // 2
+        render(this.template(), this.shadowRoot);
     }
 
     template() {
-        // TODO 3
+        // 3
         return html`
-            <!-- TODO 3 -->
+            <!-- 3 -->
+            <style>
+            .card {
+    color: #333;
+    padding: 4px;
+    border-radius: .25em;
+    border: thin solid rgba(0, 0, 0, .12);
+}
+
+.card.depth-1 {
+    box-shadow: 0 1px 3px rgba(0, 0, 0, .12), 
+                0 1px 2px rgba(0, 0, 0, .24);
+}
+
+.card.depth-2 {
+    box-shadow: 0 3px 6px rgba(0,0,0,.16), 
+                0 3px 6px rgba(0,0,0,.23);
+}
+
+.card.depth-3 {
+    box-shadow: 0 10px 20px rgba(0,0,0,.19), 
+                0 6px 6px rgba(0,0,0,.23);
+}
+
+.card.depth-4 {
+    box-shadow: 0 14px 28px rgba(0,0,0,.25), 
+                0 10px 10px rgba(0,0,0,.22);
+}
+
+.card.depth-5 {
+    box-shadow: 0 19px 38px rgba(0,0,0,.30),
+                0 15px 12px rgba(0,0,0,.22);
+}
+            </style>
             <div class=${`card depth-${this.depth}`}>
                 ${this.innerContent}
             </div>
         `;
     }
-
 }
 
 export default function () {
