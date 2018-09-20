d98180ce92c64473eb79429282213f736390157d
Igor Laborie <ilaborie@gmail.com>
2018-09-20 22:57:21 +0200

    soluce exo 0

--- native/src/exos/exo-0/exo.ts
+++ native/src/exos/exo-0/exo.ts
@@ -2,13 +2,13 @@ export default function (name: string) {
     // This is just a warm-up exercise, 
     // you just need to uncomment the code to pass the test.
 
-    // TODO 1. use the DOM api to create an HTML `DIV`
-    // const div = document.createElement('div');
+    // use the DOM api to create an HTML `DIV`
+    const div = document.createElement('div');
 
-    //  TODO 2. use the `innerHTML` to add the text `Hello XXX!` into the `DIV`.
-    // div.innerHTML = `Hello ${name}!`;
+    // use the `innerHTML` to add the text `Hello XXX!` into the `DIV`.
+    div.innerHTML = `Hello ${name}!`;
 
-    //  TODO 3. add the `DIV` into the first element with CSS class `playground`.
+    // add the `DIV` into the first element with CSS class `playground`.
     const playground = document.querySelector('.playground');
-    // playground.appendChild(div);
+    playground.appendChild(div);
 }
